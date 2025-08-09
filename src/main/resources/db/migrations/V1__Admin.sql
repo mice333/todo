6b370193366a34ef8a12a092b663117a6e89a21d
nokisev <nokisev@mail.ru>
2025-08-09 05:21:52 +0300

    Add flyway migrations.

---
+++ src/main/resources/db/migrations/V1__Admin.sql
@@ -0,0 +1,5 @@
+INSERT INTO Users (username, link) VALUES ('nokisssev', 't.me/nokisssev');
+
+INSERT INTO Tasks (title, description, priority, is_completed, user_id) VALUES 
+('Доделать CI и написать нормальные тесты', 'Использовать github actions и junit 5', 2, true, 1),
+('Подключить брокер сообщений', 'Например Apache Kafka', 1, false, 1),
